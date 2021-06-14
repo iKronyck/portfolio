669a2256f2ecb3e53577aede2d8caca6a1e1d82a
Israel Alfaro <israel.devalfaro@gmail.com>
2021-06-13 23:16:46 -0600

    :wrench: Updated images path in gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: "./src/assets/images",
       },
       __key: "images",
     },
