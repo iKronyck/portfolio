4dc941205da0a5a57d7f4ea5d3076c20a9287365
Israel Alfaro <israel.devalfaro@gmail.com>
2021-06-13 23:14:34 -0600

    :lipstick: Added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
+
   theme: {
-    extend: {},
+    backgroundColor: theme => ({
+      'primary': '#FF3847',
+      'secondary': '#0E1A2A',
+      'neutral': '#FDFDFD',
+      ...theme('colors'),
+    }),
+    screen: {
+      'x2': '320px',
+      ...defaultTheme.screens,
+    },
   },
   variants: {
     extend: {},
